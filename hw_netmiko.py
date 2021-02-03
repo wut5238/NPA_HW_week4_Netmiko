a9c14e057bf0b527e37e7aee0c79471a23df0b25
wut5238 <setthawut9915@hotmail.com>
2021-02-03 09:26:50 +0000

    Configure a loopback interface 0 of all devices

--- hw_netmiko.py
+++ hw_netmiko.py
@@ -1,15 +1,21 @@
 from netmiko import ConnectHandler
 
-device_ip = '172.31.182.1'
+device_ip = ['172.31.182.1', '172.31.182.2', '172.31.182.3', '172.31.182.4', '172.31.182.5']
 username = 'admin'
 password = 'cisco'
+loopback_ip = ['172.20.182.1', '172.20.182.2', '172.20.182.3', '172.20.182.4', '172.20.182.5']
+for ip in device_ip:
+    config = ['conf t', 'int loopback 0', 'ip add {} 255.255.255.255'.format(loopback_ip[device_ip.index(ip)]), 'end']
+    device_params = {'device_type': 'cisco_ios',
+                    'ip' : ip,
+                    'username': username,
+                    'password':password,
+                    }
 
-device_params = {'device_type': 'cisco_ios',
-                 'ip' : device_ip,
-                 'username': username,
-                 'password':password,
-                }
-
-with ConnectHandler(**device_params) as ssh:
-    result = ssh.send_command('sh ip int br')
-    print(result)
+    with ConnectHandler(**device_params) as ssh:
+        ssh.enable()
+        ssh.send_config_set(config)
+        result = ssh.send_command('sh ip int br')
+        print('============= '+ssh.send_command('sh run | sect hostname')+' =============')
+        print(result)
+        print('==========================================================================')
