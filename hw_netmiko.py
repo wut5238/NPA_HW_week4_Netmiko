d341bc32201bf5274e40a99684296130cdd6cf4f
wut5238 <setthawut9915@hotmail.com>
2021-02-10 11:49:01 +0000

    add method config_ospf

--- hw_netmiko.py
+++ hw_netmiko.py
@@ -3,7 +3,7 @@ from netmiko import ConnectHandler
 # R1-R5 auto config
 # config static route to management plane
 # config default route to nat in ospf
-# device_ip = ['172.31.182.3', '172.31.182.4', '172.31.182.5', '172.31.182.6', '172.31.182.7', '172.31.182.8', '172.31.182.9']
+
 username = 'admin'
 password = 'cisco'
 
@@ -15,17 +15,20 @@ routers = [
                 {
                     "int":"l0",
                     "ip":"172.20.182.4",
-                    "netmask":"255.255.255.255"
+                    "netmask":"255.255.255.255",
+                    "wildcard":"0.0.0.0"
                 },
                 {
                     "int":"g0/1",
                     "ip":"172.31.182.17",
-                    "netmask":"255.255.255.240"
+                    "netmask":"255.255.255.240",
+                    "wildcard":"0.0.0.15"
                 },
                 {
                     "int":"g0/2",
                     "ip":"172.31.182.33",
-                    "netmask":"255.255.255.240"
+                    "netmask":"255.255.255.240",
+                    "wildcard":"0.0.0.15"
                 }
             ]
     },
@@ -36,17 +39,20 @@ routers = [
                 {
                     "int":"l0",
                     "ip":"172.20.182.5",
-                    "netmask":"255.255.255.255"
+                    "netmask":"255.255.255.255",
+                    "wildcard":"0.0.0.0"
                 },
                 {
                     "int":"g0/1",
                     "ip":"172.31.182.18",
-                    "netmask":"255.255.255.240"
+                    "netmask":"255.255.255.240",
+                    "wildcard":"0.0.0.15"
                 },
                 {
                     "int":"g0/2",
                     "ip":"172.31.182.49",
-                    "netmask":"255.255.255.240"
+                    "netmask":"255.255.255.240",
+                    "wildcard":"0.0.0.15"
                 }
             ]
     },
@@ -57,22 +63,26 @@ routers = [
                 {
                     "int":"l0",
                     "ip":"172.20.182.6",
-                    "netmask":"255.255.255.255"
+                    "netmask":"255.255.255.255",
+                    "wildcard":"0.0.0.0"
                 },
                 {
                     "int":"g0/1",
                     "ip":"172.31.182.34",
-                    "netmask":"255.255.255.240"
+                    "netmask":"255.255.255.240",
+                    "wildcard":"0.0.0.15"
                 },
                 {
                     "int":"g0/2",
                     "ip":"172.31.182.50",
-                    "netmask":"255.255.255.240"
+                    "netmask":"255.255.255.240",
+                    "wildcard":"0.0.0.15"
                 },
                 {
                     "int":"g0/3",
                     "ip":"172.31.182.65",
-                    "netmask":"255.255.255.240"
+                    "netmask":"255.255.255.240",
+                    "wildcard":"0.0.0.15"
                 }
             ]
     },
@@ -83,12 +93,14 @@ routers = [
                 {
                     "int":"l0",
                     "ip":"172.20.182.7",
-                    "netmask":"255.255.255.255"
+                    "netmask":"255.255.255.255",
+                    "wildcard":"0.0.0.0"
                 },
                 {
                     "int":"g0/1",
                     "ip":"172.31.182.66",
-                    "netmask":"255.255.255.240"
+                    "netmask":"255.255.255.240",
+                    "wildcard":"0.0.0.15"
                 },
             ]
     },
@@ -99,12 +111,14 @@ routers = [
                 {
                     "int":"l0",
                     "ip":"172.20.182.9",
-                    "netmask":"255.255.255.255"
+                    "netmask":"255.255.255.255",
+                    "wildcard":"0.0.0.0"
                 },
                 {
                     "int":"g0/1",
                     "ip":"172.31.182.67",
-                    "netmask":"255.255.255.240"
+                    "netmask":"255.255.255.240",
+                    "wildcard":"0.0.0.15"
                 },
             ]
     }
@@ -141,4 +155,21 @@ def conf_int():
                 result = ssh.send_command('sh ip int br')
         print()
         print(result)
-conf_int()
+
+def conf_ospf():
+
+    for ip in routers:
+        device_params = {'device_type': 'cisco_ios',
+                        'ip' : ip["device_ip"],
+                        'username': username,
+                        'password':password,
+                        }
+        for num in range(0, len(ip["interfaces"])):
+            config_ospf = ['conf t', 'router ospf 1', 'network {} {} area 0'.format(ip["interfaces"][num]["ip"], ip["interfaces"][num]["wildcard"])]
+            with ConnectHandler(**device_params) as ssh:
+                ssh.enable()
+                ssh.send_config_set(config_ospf)
+                result = ssh.send_command('sh run | sect ospf')
+        print()
+        print(result)
+conf_ospf()
